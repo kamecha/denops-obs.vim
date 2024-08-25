222578c76dddfa90e4edb7c74800ddb8d0bdf2b0
kamecha <masakikameyama81@gmail.com>
2024-08-25 11:52:16 +0900

    obsのWebSocketのconnectionの基礎を書いた

--- denops/obs/main.ts
+++ denops/obs/main.ts
@@ -1,3 +1,28 @@
 import type { Entrypoint } from "jsr:@denops/std@7.0.0";
+import { is, maybe } from "jsr:@core/unknownutil@4.3.0";
+import OBSWebSocket from "npm:obs-websocket-js@5.0.6";
 
-export const main: Entrypoint = () => {};
+let obs: OBSWebSocket | undefined;
+
+export const main: Entrypoint = (denops) => {
+  denops.dispatcher = {
+    new: () => {
+      obs = new OBSWebSocket();
+    },
+    connect: async (url?: unknown, password?: unknown): Promise<void> => {
+      if (obs === undefined) {
+        return;
+      }
+      await obs.connect(
+        maybe(url, is.String),
+        maybe(password, is.String),
+      );
+    },
+    disconnect: async (): Promise<void> => {
+      if (obs === undefined) {
+        return;
+      }
+      await obs.disconnect();
+    },
+  };
+};
