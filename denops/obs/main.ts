d0505538be171eb1fcc37ecd117b7736274fb066
kamecha <masakikameyama81@gmail.com>
2024-08-27 21:25:46 +0900

    obsのundefined確認を横着した

--- denops/obs/main.ts
+++ denops/obs/main.ts
@@ -1,6 +1,6 @@
 import type { Entrypoint } from "jsr:@denops/std@7.0.0";
 import { is, maybe } from "jsr:@core/unknownutil@4.3.0";
-import OBSWebSocket from "npm:obs-websocket-js@5.0.6";
+import { OBSWebSocket } from "npm:obs-websocket-js@5.0.6";
 
 let obs: OBSWebSocket | undefined;
 
@@ -9,20 +9,17 @@ export const main: Entrypoint = (denops) => {
     new: () => {
       obs = new OBSWebSocket();
     },
-    connect: async (url?: unknown, password?: unknown): Promise<void> => {
-      if (obs === undefined) {
-        return;
-      }
-      await obs.connect(
+    connect: async (url?: unknown, password?: unknown) => {
+      await obs?.connect(
         maybe(url, is.String),
         maybe(password, is.String),
       );
     },
-    disconnect: async (): Promise<void> => {
-      if (obs === undefined) {
-        return;
-      }
-      await obs.disconnect();
+    disconnect: async () => {
+      await obs?.disconnect();
+    },
+    identified: (): unknown => {
+      return obs?.identified;
     },
   };
 };
